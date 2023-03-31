84fea0df1e5ff964bfec4ab91e43e263247ad6fa
Rupali Gupta <126675784+Rupali1520@users.noreply.github.com>
2023-03-31 11:59:36 +0530

    Update s1.sh

--- s1.sh
+++ s1.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-a="yes"
-while [$a -e "yes"]
-do
-echo "Please enter the number of days: "
-read  d
-echo "The result files are: "
+
+echo "Please enter the number of days:"
+read d
+echo "The result files are:"
 find /home/knoldus -type f -mtime +$d -print -ls
 echo "Do you want to continue or not?(yes/no)"
-read continuee
-if [$continuee=="no"]
-then
-echo "exit.."
-a="no"
-
+read answer
+if [ "$answer" == "yes" ]; then
+   './shellscript.sh'
+else
+  echo "exiting..."
+  exit 1
 fi
-done
+
